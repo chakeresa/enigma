8ddf1bcaeb9629d851a996073805ee7630016dde
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-11 16:04:17 -0600

    Add (skipped) test of encrypt method for Enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,10 @@
 class Enigma
+# def encrypt(message, key, date)
+#   # TO DO
+#   {
+#     encryption: ciphertext, # TO DO
+#     key: key,
+#     date: date
+#   }
+# end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,4 +8,16 @@ class EnigmaTest < Minitest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+  def test_encrypt_lowercase_and_spaces_only_produces_hash
+    skip
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+    # The encrypt method takes a message String as an argument. It can optionally take a Key and Date as arguments to use for encryption. If the key is not included, generate a random key. If the date is not included, use today’s date.
+end
 end
