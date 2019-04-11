d4d134b363c2195c932b13d2db69c42f0dfa01c4
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-11 16:01:33 -0600

    Initialize Enigma

---
+++ test/enigma_test.rb
@@ -0,0 +1,11 @@
+require './test/test_helper'
+
+class EnigmaTest < Minitest::Test
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+end
