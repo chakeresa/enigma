76127aee2b75dee8a6975ce3f2c9da7d34d9bfb6
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-12 10:26:42 -0600

    Add shift_count attr to ShiftGenerator

--- lib/shift_generator.rb
+++ lib/shift_generator.rb
@@ -1,4 +1,10 @@
 class ShiftGenerator
+  attr_reader :shift_count
+  
+  def initialize(shift_count = 4)
+    @shift_count = shift_count
+  end
+
   def neg_shift_array
     shift_array.map {|shift| -1 * shift}
   end
--- test/shift_generator_test.rb
+++ test/shift_generator_test.rb
@@ -2,10 +2,16 @@ require './test/test_helper'
 
 class ShiftGeneratorTest < Minitest::Test
   def setup
-    @shift_gen1 = ShiftGenerator.new
+    @shift_gen4 = ShiftGenerator.new
+    @shift_gen7 = ShiftGenerator.new(7)
   end
 
   def test_it_exists
-    assert_instance_of ShiftGenerator, @shift_gen1
+    assert_instance_of ShiftGenerator, @shift_gen4
+  end
+
+  def test_it_has_shift_count_attr_that_defaults_to_four
+    assert_equal 4, @shift_gen4.shift_count
+    assert_equal 7, @shift_gen7.shift_count
   end
 end
