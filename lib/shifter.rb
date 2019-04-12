10c7d1b038515803e1c47b1ccd684764d5808b9b
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-11 19:29:13 -0600

    Add shifted_set method to Shifter

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -4,4 +4,8 @@ class Shifter
   def initialize
     @character_set = ("a".."z").to_a << " "
   end
+
+  def shifted_set(shift)
+    @character_set.rotate(shift)
+  end
 end
--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -13,4 +13,14 @@ class ShifterTest < Minitest::Test
     expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
     assert_equal expected, @shifter.character_set
   end
+
+  def test_shifted_set_returns_rotated_character_set_given_shift
+    expected = ["c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a", "b"]
+    assert_equal expected, @shifter.shifted_set(2)
+  end
+
+  def test_shifted_set_goes_backwards_given_negative_shift
+    expected = ["y", "z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x"]
+    assert_equal expected, @shifter.shifted_set(-3)
+  end
 end
