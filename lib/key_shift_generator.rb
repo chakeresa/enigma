a3f14dab8863b65357854201b3b43df2c4c2afdf
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-11 20:17:27 -0600

    Add key_shift_array method to KeyShiftGenerator

--- lib/key_shift_generator.rb
+++ lib/key_shift_generator.rb
@@ -1,7 +1,15 @@
 class KeyShiftGenerator
   attr_reader :key
-  
+
   def initialize(key)
     @key = key
+  end
+
+  def key_shift_array
+    key_shifts = []
+    @key.chars.each_cons(2) do |number1, number2|
+      key_shifts << (number1 + number2).to_i
+    end
+    key_shifts
   end
 end
--- test/key_shift_generator_test.rb
+++ test/key_shift_generator_test.rb
@@ -2,14 +2,28 @@ require './test/test_helper'
 
 class KeyShiftGeneratorTest < Minitest::Test
   def setup
-    @key_shift_gen = KeyShiftGenerator.new("58467")
+    @key_shift_gen1 = KeyShiftGenerator.new("58467")
   end
 
   def test_it_exists
-    assert_instance_of KeyShiftGenerator, @key_shift_gen
+    assert_instance_of KeyShiftGenerator, @key_shift_gen1
   end
 
   def test_it_inits_with_a_key
-    assert_equal "58467", @key_shift_gen.key
+    assert_equal "58467", @key_shift_gen1.key
+  end
+
+  def test_key_shift_array_returns_array_of_abcd_keys
+    assert_equal [58, 84, 46, 67], @key_shift_gen1.key_shift_array
+  end
+
+  def test_key_shift_array_works_w_leading_zeroes_in_key
+    key_shift_gen2 = KeyShiftGenerator.new("00287")
+    assert_equal [0, 2, 28, 87], key_shift_gen2.key_shift_array
+  end
+
+  def test_key_shift_array_works_zeroes_in_middle_of_key
+    key_shift_gen3 = KeyShiftGenerator.new("50763")
+    assert_equal [50, 7, 76, 63], key_shift_gen3.key_shift_array
   end
 end
