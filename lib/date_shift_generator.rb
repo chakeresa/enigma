09537a956945c4b651fc29f7026c8421be65a114
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-11 20:40:56 -0600

    Initialize DateShiftGenerator with DDMMYY string as date_key ivar

--- lib/date_shift_generator.rb
+++ lib/date_shift_generator.rb
@@ -1,2 +1,7 @@
 class DateShiftGenerator
+  attr_reader :date_key
+  
+  def initialize(date_key)
+    @date_key = date_key
+  end
 end
--- test/date_shift_generator_test.rb
+++ test/date_shift_generator_test.rb
@@ -2,10 +2,14 @@ require './test/test_helper'
 
 class DateShiftGeneratorTest < Minitest::Test
   def setup
-    @date_shift_gen1 = DateShiftGenerator.new
+    @date_shift_gen1 = DateShiftGenerator.new("011218")
   end
 
   def test_it_exists
     assert_instance_of DateShiftGenerator, @date_shift_gen1
+  end
+
+  def test_it_inits_with_a_date_key_string
+    assert_equal "011218", @date_shift_gen1.date_key
   end
 end
