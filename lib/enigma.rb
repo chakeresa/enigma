9e307c9f587de789bb164544c9e5c8e8e3cd269d
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-11 22:20:18 -0600

    Add encrypt method to Enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,7 +4,7 @@ require_relative 'key_shift_generator'
 require_relative 'date_shift_generator'
 
 class Enigma
-  def generate_random_key
+  def random_key
     random_number_string = rand(99999).to_s
     ('%5s' % random_number_string).gsub(" ", "0")
   end
@@ -13,14 +13,18 @@ class Enigma
     Date.today.strftime("%d%m%y")
   end
 
-# def encrypt(message, key, date)
-#   # TO DO
-#   {
-#     encryption: ciphertext, # TO DO
-#     key: key,
-#     date: date
-#   }
-# end
+def encrypt(message, key = random_key, date = todays_date)
+  # TO DO: shorten up
+  ciphertext = ""
+  key_shift_ary = KeyShiftGenerator.new(key).key_shift_array
+  date_shift_ary = DateShiftGenerator.new(date).date_shift_array
+  message.each_char.with_index do |char, index_of_char|
+    abcd_index = index_of_char % 4
+    shifted = Shifter.shift_letter(char, key_shift_ary[abcd_index])
+    ciphertext << Shifter.shift_letter(shifted, date_shift_ary[abcd_index])
+  end
+  {encryption: ciphertext, key: key, date: date}
+end
 
 # def decrypt(ciphertext, key, date)
 #   # TO DO
