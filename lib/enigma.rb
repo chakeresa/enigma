a81aa2959803730b42792e22412590a8cb7ee4d1
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-11 22:24:50 -0600

    Add generate_ciphertext helper method for Enigma#encrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -13,18 +13,22 @@ class Enigma
     Date.today.strftime("%d%m%y")
   end
 
-def encrypt(message, key = random_key, date = todays_date)
-  # TO DO: shorten up
-  ciphertext = ""
-  key_shift_ary = KeyShiftGenerator.new(key).key_shift_array
-  date_shift_ary = DateShiftGenerator.new(date).date_shift_array
-  message.each_char.with_index do |char, index_of_char|
-    abcd_index = index_of_char % 4
-    shifted = Shifter.shift_letter(char, key_shift_ary[abcd_index])
-    ciphertext << Shifter.shift_letter(shifted, date_shift_ary[abcd_index])
+  def generate_ciphertext(message, key_shift_ary, date_shift_ary)
+    ciphertext = ""
+    message.each_char.with_index do |char, index_of_char|
+      abcd_index = index_of_char % 4
+      shifted = Shifter.shift_letter(char, key_shift_ary[abcd_index])
+      ciphertext << Shifter.shift_letter(shifted, date_shift_ary[abcd_index])
+    end
+    ciphertext
   end
-  {encryption: ciphertext, key: key, date: date}
-end
+
+  def encrypt(message, key = random_key, date = todays_date)
+    key_shift_ary = KeyShiftGenerator.new(key).key_shift_array
+    date_shift_ary = DateShiftGenerator.new(date).date_shift_array
+    ciphertext = generate_ciphertext(message, key_shift_ary, date_shift_ary)
+    {encryption: ciphertext, key: key, date: date}
+  end
 
 # def decrypt(ciphertext, key, date)
 #   # TO DO
