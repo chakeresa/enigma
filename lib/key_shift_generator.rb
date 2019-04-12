14ad7bd58d79b365d408de10a2832f46a244f440
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-11 20:04:21 -0600

    Add key attr to init of KeyShiftGenerator

--- lib/key_shift_generator.rb
+++ lib/key_shift_generator.rb
@@ -1,2 +1,7 @@
 class KeyShiftGenerator
+  attr_reader :key
+  
+  def initialize(key)
+    @key = key
+  end
 end
--- test/key_shift_generator_test.rb
+++ test/key_shift_generator_test.rb
@@ -2,10 +2,14 @@ require './test/test_helper'
 
 class KeyShiftGeneratorTest < Minitest::Test
   def setup
-    @key_shift_gen = KeyShiftGenerator.new
+    @key_shift_gen = KeyShiftGenerator.new("58467")
   end
 
   def test_it_exists
     assert_instance_of KeyShiftGenerator, @key_shift_gen
+  end
+
+  def test_it_inits_with_a_key
+    assert_equal "58467", @key_shift_gen.key
   end
 end
