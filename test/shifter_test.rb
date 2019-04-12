0d6e19eb8ad5f5fb146aa612ed3ac8b7aae13062
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-11 19:22:29 -0600

    Add character_set attribute to Shifter

--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -8,4 +8,9 @@ class ShifterTest < Minitest::Test
   def test_it_exists
     assert_instance_of Shifter, @shifter
   end
+
+  def test_it_inits_with_character_set_of_a_to_z_and_space
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, @shifter.character_set
+  end
 end
