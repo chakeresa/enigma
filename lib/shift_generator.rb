9db878b72b145b34e4c56f7439bf863729c7a6c1
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-12 10:37:13 -0600

    Change shift_count ivar to SHIFT_COUNT class constant of ShiftGenerator

--- lib/shift_generator.rb
+++ lib/shift_generator.rb
@@ -1,9 +1,5 @@
 class ShiftGenerator
-  attr_reader :shift_count
-  
-  def initialize(shift_count = 4)
-    @shift_count = shift_count
-  end
+  SHIFT_COUNT = 4
 
   def neg_shift_array
     shift_array.map {|shift| -1 * shift}
--- test/shift_generator_test.rb
+++ test/shift_generator_test.rb
@@ -2,16 +2,14 @@ require './test/test_helper'
 
 class ShiftGeneratorTest < Minitest::Test
   def setup
-    @shift_gen4 = ShiftGenerator.new
-    @shift_gen7 = ShiftGenerator.new(7)
+    @shift_gen1 = ShiftGenerator.new
   end
 
   def test_it_exists
-    assert_instance_of ShiftGenerator, @shift_gen4
+    assert_instance_of ShiftGenerator, @shift_gen1
   end
 
-  def test_it_has_shift_count_attr_that_defaults_to_four
-    assert_equal 4, @shift_gen4.shift_count
-    assert_equal 7, @shift_gen7.shift_count
+  def test_it_has_shift_count_const_equal_to_four
+    assert_equal 4, ShiftGenerator::SHIFT_COUNT
   end
 end
