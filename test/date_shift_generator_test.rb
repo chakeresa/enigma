23f7c18d4a059163bd3a640034096a399852b61c
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-12 11:48:38 -0600

    Add tests for superclass methods in DateShiftGeneratorTest

--- test/date_shift_generator_test.rb
+++ test/date_shift_generator_test.rb
@@ -9,6 +9,23 @@ class DateShiftGeneratorTest < Minitest::Test
     assert_instance_of DateShiftGenerator, @date_shift_gen1
   end
 
+  def test_it_has_a_key_length_constant
+    assert_equal 6, DateShiftGenerator::KEY_LENGTH
+  end
+
+  def test_init_adds_padding_to_key_input
+    skip
+    # TO DO: fix ShiftGenerator#validate_key_input so it pads to KEY_LENGTH instead of hard-entered 5
+    assert_equal "011419", DateShiftGenerator.new(11419).key
+    assert_equal "011419", DateShiftGenerator.new("11419").key
+  end
+
+  def test_init_throws_error_if_key_input_too_long_or_non_numerical
+    skip
+    assert_equal # TO DO: throw error, DateShiftGenerator.new("12042019").key
+    assert_equal # TO DO: throw error, DateShiftGenerator.new("apr11").key
+  end
+
   def test_it_inits_with_a_date_key_string
     assert_equal "011218", @date_shift_gen1.key
   end
