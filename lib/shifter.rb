ae56c3ac013d2c1b5fb701a83d34e03cc5fad0a2
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-11 19:54:04 -0600

    Make CHARACTER_SET a constant instead of an instance variable

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -1,17 +1,13 @@
 class Shifter
-  attr_reader :character_set
+  CHARACTER_SET = ("a".."z").to_a << " "
 
-  def initialize
-    @character_set = ("a".."z").to_a << " "
+  def self.shifted_set(shift)
+    CHARACTER_SET.rotate(shift)
   end
 
-  def shifted_set(shift)
-    @character_set.rotate(shift)
-  end
-
-  def shift_letter(letter, shift)
+  def self.shift_letter(letter, shift)
     letter.downcase!
-    letter_position = @character_set.find_index(letter)
+    letter_position = CHARACTER_SET.find_index(letter)
     letter_position.nil? ? letter : shifted_set(shift)[letter_position]
   end
 end
--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -9,30 +9,30 @@ class ShifterTest < Minitest::Test
     assert_instance_of Shifter, @shifter
   end
 
-  def test_it_inits_with_character_set_of_a_to_z_and_space
+  def test_it_has_character_set_constant_ary_with_a_to_z_and_space
     expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
-    assert_equal expected, @shifter.character_set
+    assert_equal expected, Shifter::CHARACTER_SET
   end
 
   def test_shifted_set_returns_rotated_character_set_given_shift
     expected = ["c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a", "b"]
-    assert_equal expected, @shifter.shifted_set(2)
+    assert_equal expected, Shifter.shifted_set(2)
   end
 
   def test_shifted_set_goes_backwards_given_negative_shift
     expected = ["y", "z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x"]
-    assert_equal expected, @shifter.shifted_set(-3)
+    assert_equal expected, Shifter.shifted_set(-3)
   end
 
   def test_shift_letter_returns_new_letter_given_orig_lowercase_letter_and_shift
-    assert_equal "f", @shifter.shift_letter("d", 2)
+    assert_equal "f", Shifter.shift_letter("d", 2)
   end
 
   def test_shift_letter_returns_new_letter_given_orig_uppercase_letter_and_shift
-    assert_equal "w", @shifter.shift_letter("Z", -3)
+    assert_equal "w", Shifter.shift_letter("Z", -3)
   end
 
   def test_shift_letter_returns_new_letter_given_orig_uppercase_letter_and_shift
-    assert_equal "&", @shifter.shift_letter("&", 2)
+    assert_equal "&", Shifter.shift_letter("&", 2)
   end
 end
