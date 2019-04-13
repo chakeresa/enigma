abf9c81402a97b839d1e0a9cc256c5bd94ab6418
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-13 13:49:49 -0600

    Refactor init of ShiftGenerator (key became key_input & formatted_key)

--- lib/shift_generator.rb
+++ lib/shift_generator.rb
@@ -2,22 +2,22 @@ class ShiftGenerator
   SHIFT_COUNT = 4
   KEY_LENGTH = 7
 
-  attr_reader :key
+  attr_reader :key_input, :formatted_key
 
   def initialize(key_input)
-    @key = validate_key_input(key_input)
+    @key_input = key_input
   end
 
-  def validate_key_input(key_input)
-    all_numerical = key_input.to_s.scan(/\d/).length == key_input.to_s.length
-    if !all_numerical || key_input.to_s.length > KEY_LENGTH
+  def validate_key_input
+    all_numerical = @key_input.to_s.scan(/\d/).length == @key_input.to_s.length
+    if !all_numerical || @key_input.to_s.length > KEY_LENGTH
       raise "Key input should be numerical and #{KEY_LENGTH} digits at most."
       # TO DO: throw error (should be numerical & less than KEY_LENGTH digits)
     else
       format = '%' + '5' + 's'
       # format = '%' + KEY_LENGTH.to_s + 's'
       # TO DO: ^ uncomment & deal with fallout from this change -- may need KEY_LENGTH constant (should pull from subclass not ShiftGenerator itself)
-      (format % key_input.to_s).gsub(" ", "0")
+      @formatted_key = (format % @key_input.to_s).gsub(" ", "0")
     end
   end
 
--- test/shift_generator_test.rb
+++ test/shift_generator_test.rb
@@ -16,4 +16,25 @@ class ShiftGeneratorTest < Minitest::Test
   def test_it_has_dummy_key_length_const_equal_to_seven
     assert_equal 7, ShiftGenerator::KEY_LENGTH
   end
+
+  def test_it_has_key_input_upon_init
+    assert_equal 42, @shift_gen1.key_input
+  end
+
+  def test_validate_key_input_returns_padded_key_if_valid
+    assert_equal "0000042", @shift_gen1.validate_key_input
+    assert_equal "0000042", @shift_gen1.formatted_key
+  end
+
+  def test_validate_key_input_throws_error_if_key_input_too_long_or_nonnumerical
+    error_message = "Key input should be numerical and 7 digits at most."
+    err1 = assert_raises(RuntimeError) do
+      KeyShiftGenerator.new("478884546").validate_key_input
+    end
+    assert_equal error_message, err1.message
+    err2 = assert_raises(RuntimeError) do
+      KeyShiftGenerator.new("hi7").validate_key_input
+    end
+    assert_equal error_message, err2.message
+  end
 end
